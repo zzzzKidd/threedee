8470036e0a145c6f69e329cab6d7bb5828189a66
Klaus Reimer <k@ailis.de>
2010-06-19 10:24:00 +0200

    Added base scene reader.

---
+++ threedee-core/src/main/java/de/ailis/threedee/reader/SceneReader.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2010 Klaus Reimer <k@ailis.de>
+ * See LICENSE.txt for licensing information.
+ */
+
+package de.ailis.threedee.reader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import de.ailis.threedee.entities.Scene;
+import de.ailis.threedee.io.resources.ResourceProvider;
+
+
+/**
+ * Abstract scene reader providing the base functionality a scene reader
+ * must have.
+ *
+ * @author Klaus Reimer (k@ailis.de)
+ */
+
+public abstract class SceneReader
+{
+    /** The resource provider */
+    protected ResourceProvider resourceProvider;
+
+
+    /**
+     * Creates a new model reader.
+     *
+     * @param resourceProvider
+     *            The resource provider to use
+     */
+
+    public SceneReader(final ResourceProvider resourceProvider)
+    {
+        this.resourceProvider = resourceProvider;
+    }
+
+
+    /**
+     * Reads a scene from the specified resource.
+     *
+     * @param filename
+     *            The filename of the scene to read
+     * @return The loaded scene
+     * @throws IOException
+     *             When read fails
+     */
+
+    public Scene read(final String filename) throws IOException
+    {
+        final InputStream stream = this.resourceProvider.openForRead(filename);
+        try
+        {
+            return read(stream);
+        }
+        finally
+        {
+            stream.close();
+        }
+    }
+
+
+    /**
+     * Reads a scene from the specified stream.
+     *
+     * @param stream
+     *            The stream from which to load the scene
+     * @return The loaded scene
+     * @throws IOException
+     *             When read fails
+     */
+
+    public abstract Scene read(final InputStream stream) throws IOException;
+}
